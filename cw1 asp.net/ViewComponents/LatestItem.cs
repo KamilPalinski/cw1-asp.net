4afbe93bfd603ca9ea4fd875a5e76f529f775f22
Kamil <kam.palinski@gmail.com>
2020-12-09 11:22:45 +0100

    Razor Lab 4

---
+++ cw1 asp.net/ViewComponents/LatestItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using cw1_asp.net.Database;
+
+namespace cw1_asp.net.ViewComponents
+{
+    public class LatestItem : ViewComponent
+    {
+        private readonly ExchangesDbContext _dbContext;
+
+        public LatestItem(ExchangesDbContext dBContext)
+        {
+            _dbContext = dBContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var latestItem = _dbContext.Items.OrderByDescending(x => x.Id).First();
+
+            return View("Index", latestItem);
+        }
+    }
+}
+
